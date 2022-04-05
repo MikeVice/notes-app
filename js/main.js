2d03c90d683d6a9382186e1205e535d96ffbf26e
MikeVice <iastapov1@gmail.com>
2022-04-05 23:49:56 +0300

    add functions of edit and archive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// Obj = {
-//  list : activeNotes;
-//  archive : archiveNotes;
-//}
 // let notes = [{
 //     name: '',
 //     dateAdded: '',
@@ -12,11 +8,53 @@
 
 
 displayNotes();
-var addBtn = document.getElementById('addBtn');
+let addBtn = document.getElementById('addBtn');
+let edtBtn = document.getElementById('edtBtn');
+let archiveBtn = document.getElementById('archiveBtn');
+let openBtn = document.getElementById('openBtn');
+let currIndex = 0;
 let allNotesObj = {
     list : [],
     archive : []
 };
+let archiveStatus = 1;
+archiveBtn.addEventListener('click',function(){
+    displayNotes(archiveStatus);
+    if (archiveStatus == 1) {
+        archiveBtn.innerHTML = '<i class="bi bi-archive-fill"></i>';
+        openBtn.style.display = "none";
+        archiveStatus = 0;
+    } else {
+        archiveBtn.innerHTML = '<i class="bi bi-archive"></i>';
+        openBtn.style.display = "block";
+        archiveStatus = 1;
+    }
+});
+
+edtBtn.addEventListener('click',function(){
+    let notesObj;
+	let notesString = localStorage.getItem('notes');
+	
+	if(notesString == null){
+		notesObj = [];
+	}
+	else{
+		notesObj = JSON.parse(notesString).list;
+	}
+    let noteName = document.getElementById('noteName');
+    let noteCategory = document.getElementById('noteCategory');
+    let noteContent = document.getElementById('noteContent');
+    let tempObj = { name: noteName.value, dateAdded: notesObj[currIndex].dateAdded, category: noteCategory.value, content: noteContent.value };
+	notesObj[currIndex]=tempObj;
+    allNotesObj.list = notesObj;
+    addBtn.style.display = "block";
+    edtBtn.style.display = "none";
+	localStorage.setItem('notes',JSON.stringify(allNotesObj));
+    noteName.value = "";
+    noteCategory.value = "";
+    noteContent.value = "";
+    displayNotes();
+});
 
 // below event listener will add user input into the local storage
 addBtn.addEventListener('click',function(){
@@ -51,7 +89,8 @@ addBtn.addEventListener('click',function(){
 	displayNotes();
 });
 
-function displayNotes(){
+function displayNotes(source){
+    if (!source) source = 0;
 	let notesObj;
 	let notesString = localStorage.getItem('notes');
 	
@@ -59,7 +98,11 @@ function displayNotes(){
 		notesObj = [];
 	}
 	else{
-		notesObj = JSON.parse(notesString).list;
+        if (source == 0){
+            notesObj = JSON.parse(notesString).list;
+        } else {
+            notesObj = JSON.parse(notesString).archive;
+        }
 	}
 	
 	let html = '';
@@ -72,7 +115,7 @@ function displayNotes(){
                     <td>${element.category}</td>
                     <td class="cell-collapse">${element.content.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</td>
                     <td>03/5/2022, 05/5/2022</td>
-                    <td class="text-right"><span onclick=editNote(${index})><i data-toggle="modal" data-target="#exampleModal"  class="bi bi-pencil"></i></span> <span><i class="bi bi-archive"></i></span> <span onclick=deleteNote(${index})><i class="bi bi-trash"></i></span></td>
+                    <td class="text-right"><span onclick=editeNote(${index})><i data-toggle="modal" data-target="#exampleModal"  class="bi bi-pencil"></i></span> <span><i class="bi bi-archive"></i></span> <span onclick=deleteNote(${index})><i class="bi bi-trash"></i></span></td>
                 </tr>
 			`;
 	});
@@ -105,3 +148,30 @@ function deleteNote(index){
 	
 	displayNotes();
 }
+
+function editeNote(index){
+	let notesObj;
+	let notesString = localStorage.getItem('notes');
+	
+	if(notesString == null){
+		notesObj = [];
+	}
+	else{
+		notesObj = JSON.parse(notesString).list;
+	}
+	var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+    myModal.show();
+    edtBtn.style.display = "block";
+    addBtn.style.display = "none";
+
+    let noteName = document.getElementById('noteName');
+    let noteCategory = document.getElementById('noteCategory');
+    let noteContent = document.getElementById('noteContent');
+
+    noteName.value = notesObj[index].name;
+    noteCategory.value = notesObj[index].category;
+    noteContent.value = notesObj[index].content;
+
+    currIndex = index;
+}
+
